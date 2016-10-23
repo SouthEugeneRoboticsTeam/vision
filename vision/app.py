98e7a47375dddb45d9e3f2707da59ccc30b6f15e
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-10-22 20:06:57 -0700

    Reorder functions

--- vision/app.py
+++ vision/app.py
@@ -20,7 +20,74 @@ class Vision:
 		else:
 			self.run_video()
 
+	def run_image(self):
+		im = cv2.imread(self.image)
+
+		im_rect, im_mask = self.draw_images(im)
+
+		# Show the images
+		cv2.imshow("Original", im_rect)
+		cv2.imshow("Mask", im_mask)
+
+		cv2.waitKey(0)
+
+		cv2.destroyAllWindows()
+
+	def run_video(self):
+		camera = cv2.VideoCapture(0)
+
+		while(True):
+			# Read image from file
+			(ret, im) = camera.read()
+
+			if ret:
+				# check if they wanted to draw images or not
+				im_rect, im_mask = self.draw_images(im)
+
+				# Show the images
+				cv2.imshow("Original", im_rect)
+				cv2.imshow("Mask", im_mask)
+
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+			else:
+				break
+
+		camera.release()
+		cv2.destroyAllWindows()
+
 	def process_image(self, im):
+		# Get image height and width
+		height, width = im.shape[:2]
+
+		# Create mask
+		im_mask = cv2.inRange(im, self.lower, self.upper)
+
+		# Get largest blob
+		largest = cv_utils.get_largest(im_mask)
+
+		offset_x = 0
+		offset_y = 0
+
+		if largest is not False:
+			# Get x, y, width, height of goal
+			x, y, w, h = cv2.boundingRect(largest)
+
+			# Get area of largest blob
+			largest_area = w * h
+
+			if largest_area > self.args["min_area"]:
+				# Find center of goal
+				center_x = int(0.5 * (x + (x + w)))
+				center_y = int(0.5 * (y + (y + h)))
+
+				# Find pixels away from center
+				offset_x = int(width/2 - center_x) * -1
+				offset_y = int(height/2 - center_y)
+
+		return offset_x, offset_y
+
+	def draw_images(self, im):
 		# Get image height and width
 		height, width = im.shape[:2]
 
@@ -62,38 +129,3 @@ class Vision:
 		cv_utils.draw_crosshair(im_rect, width, height, (0, 0, 0), 2)
 
 		return im_rect, im_mask
-
-	def run_image(self):
-		im = cv2.imread(self.image)
-
-		im_rect, im_mask = self.process_image(im)
-
-		# Show the images
-		cv2.imshow("Original", im_rect)
-		cv2.imshow("Mask", im_mask)
-
-		cv2.waitKey(0)
-
-		cv2.destroyAllWindows()
-
-	def run_video(self):
-		camera = cv2.VideoCapture(0)
-
-		while(True):
-			# Read image from file
-			(ret, im) = camera.read()
-
-			if ret:
-				im_rect, im_mask = self.process_image(im)
-
-				# Show the images
-				cv2.imshow("Original", im_rect)
-				cv2.imshow("Mask", im_mask)
-
-				if cv2.waitKey(1) & 0xFF == ord('q'):
-					break
-			else:
-				break
-
-		self.camera.release()
-		cv2.destroyAllWindows()
