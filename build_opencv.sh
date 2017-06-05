158288f01bc7f67ecfe9b94d86fa7acf55c60272
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-06-04 17:34:04 -0700

    fix

--- build_opencv.sh
+++ build_opencv.sh
@@ -1,23 +1,20 @@
-ls
-ls opencv
-ls opencv/build
-files=(opencv/build/*)
-if [ -n "$(ls -A opencv/build)" ]
+if [ -n "$(ls -A opencv/build)" ];
 then
-    echo "cache exists"
+    cd opencv;
+    git init;
+    git remote add origin https://github.com/Itseez/opencv.git;
+    git fetch origin 2.4;
+    git checkout origin/2.4;
+    ls;
+    cd build;
+    ls;
 else
-    rm -r opencv
+    rm -r opencv;
+    git clone https://github.com/Itseez/opencv.git;
+    cd opencv;
+    git checkout 2.4;
+    mkdir build;
+    cd build;
+    cmake -DCMAKE_INSTALL_PREFIX=/usr ..;
+    make -j8;
 fi
-git clone https://github.com/Itseez/opencv.git
-cd opencv
-git checkout 2.4
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
-make -j8
-sudo make install
-cd ../../
-export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/site-packages/
-ls
-ls opencv
-ls opencv/build
