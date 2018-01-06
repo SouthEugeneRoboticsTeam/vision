004fa1651afa8a10066d927ef9b33fa0d3b52a12
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-05 22:45:52 -0800

    Update branch

--- vision/utils.py
+++ vision/utils.py
@@ -6,21 +6,21 @@ import sys
 
 
 def get_args():
-	default_config = []
+    default_config = []
 
-	if '-cf' not in sys.argv and '--config' not in sys.argv:
-		default_config = [os.getenv('VISION_CONFIG', os.path.join(os.path.dirname(__file__), '../config/config.ini'))]
+    if '-cf' not in sys.argv and '--config' not in sys.argv:
+        default_config = [os.getenv('VISION_CONFIG', os.path.join(os.path.dirname(__file__), '../config/config.ini'))]
 
-	parser = configargparse.ArgParser(default_config_files=default_config, auto_env_var_prefix='VISION_')
+    parser = configargparse.ArgParser(default_config_files=default_config, auto_env_var_prefix='VISION_')
 
-	parser.add_argument("-i", "--image", help="path to image")
-	parser.add_argument("-s", "--sources", type=list, default=[0, 1], help="video sources (default=[0,1])")
-	parser.add_argument("-d", "--display", action="store_true", help="display results of processing in a new window")
-	parser.add_argument("-ip", "--roborio-ip", help="the ip address of the roboRIO")
-	parser.add_argument("-ma", "--min-area", type=int, help="minimum area for blobs")
-	parser.add_argument("-mx", "--max-area", type=int, help="maximum area for blobs")
-	parser.add_argument("-lt", "--lower-color", action="append", nargs="+", type=int, help="lower color threshold for BGR values")
-	parser.add_argument("-ut", "--upper-color", action="append", nargs="+", type=int, help="upper color threshold for BGR values")
-	parser.add_argument("-v", "--verbose", action="store_true", help="for debugging, prints useful values")
+    parser.add_argument("-i", "--image", help="path to image")
+    parser.add_argument("-s", "--sources", type=list, default=[0, 1], help="video sources (default=[0,1])")
+    parser.add_argument("-d", "--display", action="store_true", help="display results of processing in a new window")
+    parser.add_argument("-ip", "--roborio-ip", help="the ip address of the roboRIO")
+    parser.add_argument("-ma", "--min-area", type=int, help="minimum area for blobs")
+    parser.add_argument("-mx", "--max-area", type=int, help="maximum area for blobs")
+    parser.add_argument("-lt", "--lower-color", action="append", nargs="+", type=int, help="lower color threshold for BGR values")
+    parser.add_argument("-ut", "--upper-color", action="append", nargs="+", type=int, help="upper color threshold for BGR values")
+    parser.add_argument("-v", "--verbose", action="store_true", help="for debugging, prints useful values")
 
-	return vars(parser.parse_args())
+    return vars(parser.parse_args())
